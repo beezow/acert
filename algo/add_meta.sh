8aa845e51d0d6454ef8233d89c254ce3752ad6fc
beezow <zbeezow@gmail.com>
2020-02-29 23:51:43 -0600

    passes all metadata through

--- algo/add_meta.sh
+++ algo/add_meta.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-ffmpeg -i $1 -f ffmetadata -i .tmphash -c copy -map_metadata 1 signed.mkv
+ffmpeg -i $1 -f ffmetadata -i .tmphash -c copy -map_metadata 1 -movflags use_metadata_tags signed.mkv
 
